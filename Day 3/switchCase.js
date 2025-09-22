c3fd88c5cdaf83c52b11a8260db8a644cd35eb33
Divyam Mukesh Oswal <divyam9850400550@gmail.com>
2025-09-22 11:50:00 +0530

    added switchCase

---
+++ Day 3/switchCase.js
@@ -0,0 +1,9 @@
+switch(true){ //check the fist true condition
+    case 19 < 6:
+        console.log("hello")
+        break
+    case 10 > 6:
+        console.log("hey")
+        break
+    default: console.log("Invalid")
+}
